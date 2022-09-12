a50ffe8ce4624b69c245c8ab311fb59d6c333e2a
roska <juharai@gmail.com>
2022-09-12 17:12:55 +0300

    New project with create-react-app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Juha Raitanen
+        </p>
+        <a href="https://roska.github.io">https://roska.github.io</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
